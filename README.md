38c60a880dc5ad846e45021fbe51c4e3881c345b
Shantanoo 'Shan' Desai <shantanoo.desai@gmail.com>
2022-04-12 21:16:18 +0200

    chore: bump stable version v1.0.0
    Signed-off-by: Shantanoo 'Shan' Desai <shantanoo.desai@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# influxdb-docker-init-poc
+# Initialize and Provision your InfluxDBv2.x Container using Docker Init Containers
+
 Proof-of-Concept of Provisioning your InfluxDBv2.x container through Docker Init Containers using Compose files
+
+InfluxDB has already of provisioning the database by using scripts as volume mounts in the `/docker-entrypoint-initdb.d`
+directory. See [Docker Hub Documentation for InfluxDB under section Custom Initialization Scripts][1].
+
+This Repository is a proof-of-concept that once achieve similar initialization via the docker init containers in compose file
+
+## Usage
+
+```bash
+docker compose up
+```
+
+Should provide you verbose logs that the default initialization user and a custom user are created via the `init-db`
+docker init containers.
+
+## Results
+
+- Login into the UI at `http://localhost:8086` using the default admin credentials (see `influxdb.env`)
+
+- Once logged into UI click User Icon on the left-side panel you will observe the provisioned Organization in the `bootstrap-influxdb.sh`
+script
+
+![Provisioned Organizations in InfluxDB UI main page](./.github/assets/influxdb_ui_home_orgs.png)
+
+- Select the `coreOrg` organization and click on the user icon on left-side panel and click on __Members__, you also see the provisioned
+members
+
+![Provisioned User in InfluxDB Org coreOrg](./.github/assets/influxdb_ui_user.png)
+
+## Docker Init Container in Compose
+
+> NOTE: this feature is similar to [Kubernetes Init Containers][1], which is available for
+> Docker Compose since version 1.29.
+
+The Init Container can initialize your container by using the `depends_on` spec. Depending on the
+intialization process, you can set three conditions of the container's state you wish to initialize:
+
+- `service_started`
+- `service_healthy`
+- `service_completed_sucessfully`
+
+Unfortunately, this feature is yet to be documented. However, some resources to look into:
+
+1. [Feature Request Issue on Docker Compose ][2]
+2. [Pending Pull-Request for Documentation of Init Containers][3]
+3. [StackExchange Query with a _possible_ example][4]
+
+[1]: https://hub.docker.com/_/influxdb/
+[2]: https://github.com/docker/compose/issues/6855
+[3]: https://github.com/docker/docker.github.io/issues/12633
+[4]: https://stackoverflow.com/questions/70322031/does-docker-compose-support-init-container
---
+++ bootstraper/bootstrap-influxdb.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# Copyright 2022 Shantanoo "Shan" Desai <shantanoo.desai@gmail.com>
+
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+
+#        http://www.apache.org/licenses/LICENSE-2.0
+
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+# Bootstrapping Script for InfluxDBv2.1 executed by the Docker Init Container
+
+set -e
+
+# Set Default Values if not provided by Env Vars
+DEFAULT_INFLUXDB_BUCKET=initBucket
+DEFAULT_INFLUXDB_ORG=initOrg
+DEFAULT_INFLUXDB_USERNAME=initAdmin
+DEFAULT_INFLUXDB_PASSWORD=securePassword
+DEFAULT_INFLUXDB_ADMIN_TOKEN=initToken
+
+# Set the environment variables from the init container
+# If not provided use default values
+DOCKER_INFLUXDB_INIT_ADMIN_TOKEN=${DOCKER_INFLUXDB_INIT_ADMIN_TOKEN:-DEFAULT_INFLUXDB_ADMIN_TOKEN}
+DOCKER_INFLUXDB_INIT_BUCKET=${DOCKER_INFLUXDB_INIT_BUCKET:-DEFAULT_INFLUXDB_BUCKET}
+DOCKER_INFLUXDB_INIT_ORG=${DOCKER_INFLUXDB_INIT_ORG:-DEFAULT_INFLUXDB_ORG}
+DOCKER_INFLUXDB_INIT_PASSWORD=${DOCKER_INFLUXDB_INIT_PASSWORD:-DEFAULT_INFLUXDB_PASSWORD}
+DOCKER_INFLUXDB_INIT_USERNAME=${DOCKER_INFLUXDB_INIT_USERNAME:-DEFAULT_INFLUXDB_USERNAME}
+
+## Custom Setup
+CUSTOM_BUCKET=coreData
+CUSTOM_ORG=coreOrg
+CUSTOM_USER=edgeCoreUser
+CUSTOM_PASSWORD=edgeCoreUsersPassword
+
+# Setup InfluxDB using the `setup` command
+influx setup --bucket ${DOCKER_INFLUXDB_INIT_BUCKET} -t ${DOCKER_INFLUXDB_INIT_ADMIN_TOKEN} -o ${DOCKER_INFLUXDB_INIT_ORG}  --username="${DOCKER_INFLUXDB_INIT_USERNAME}" --password="${DOCKER_INFLUXDB_INIT_PASSWORD}" --host=http://influxdb:8086 -f
+
+
+# Create a Custom Organization
+influx org create -n ${CUSTOM_ORG} --host=http://influxdb:8086 -t ${DOCKER_INFLUXDB_INIT_ADMIN_TOKEN}
+
+# Create a Custom Bucket, in the Org with retention policy of 24 hours
+influx bucket create -n ${CUSTOM_BUCKET} -o ${CUSTOM_ORG} -r 24h --host=http://influxdb:8086 -t ${DOCKER_INFLUXDB_INIT_ADMIN_TOKEN}
+
+# Create a Custom User 
+influx user create -n ${CUSTOM_USER} -p ${CUSTOM_PASSWORD} -o ${CUSTOM_ORG} --host=http://influxdb:8086 -t ${DOCKER_INFLUXDB_INIT_ADMIN_TOKEN}
